c3c63109e737530ad576bc65156768be745588ce
lewis <lewishsh@gmail.com>
2018-02-27 00:40:42 -0500

    commit

---
+++ auto/pset3.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "pset3"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=1.25in")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "microtype"
+    "enumitem"
+    "amsmath"
+    "amssymb"
+    "geometry")
+   (TeX-add-symbols
+    "R"
+    "N"))
+ :latex)
+
