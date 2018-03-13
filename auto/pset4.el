6065d8513faeba88080de602e4664605d6b8d44d
lewis <lewishsh@gmail.com>
2018-03-13 15:36:00 -0400

    commit

---
+++ auto/pset4.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "pset4"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=1.25in")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "microtype"
+    "enumitem"
+    "amsmath"
+    "amssymb"
+    "geometry")
+   (TeX-add-symbols
+    "R"
+    "ran"
+    "N"))
+ :latex)
+
